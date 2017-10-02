f12c3c20b756d6262d4c7237fa74c0bd520319c9
David Glasser <glasser@davidglasser.net>
2017-10-02 16:32:06 -0700

    xxx

--- loop.sh
+++ loop.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+while true; do
+  time
+  echo inserting
+  mongo lighthouse.4.mongolayer.com:10615/testdb -u testuser -p testpass --eval 'db.c.insert({x: "hi"})'
+  echo inserted
+  sleep 1
+done
--- run_app.sh
+++ run_app.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
-url='mongodb://testuser:testpass@lighthouse.5.mongolayer.com:10615,lighthouse.4.mongolayer.com:10615/testdb?replicaSet=set-59d2c65e61320f48ba000da1'
+prefix='mongodb://testuser:testpass@lighthouse.5.mongolayer.com:10615,lighthouse.4.mongolayer.com:10615'
+suffix='replicaSet=set-59d2c65e61320f48ba000da1'
 
 cd "$(dirname "$0")"/cpapp || exit
 
-MONGO_URL="$url" MONGO_OPLOG_URL="$url" meteor
+MONGO_URL="${prefix}/testdb?${suffix}" \
+         MONGO_OPLOG_URL="${prefix}/local?authSource=admin&${suffix}" meteor
