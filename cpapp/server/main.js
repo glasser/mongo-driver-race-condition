4b8f9f971c18822e0a52e0e04371bcf3fa3cbc81
David Glasser <glasser@davidglasser.net>
2017-10-02 16:39:47 -0700

    clean up at start

--- cpapp/server/main.js
+++ cpapp/server/main.js
@@ -2,6 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 const c = new Mongo.Collection('c');
+console.log("Cleaning up collection");
+c.remove({});
+console.log("Observing");
 const o = c.find().observeChanges({
   added(id, fields) {
     console.log("add", id, fields);
