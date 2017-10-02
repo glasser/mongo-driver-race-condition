5b25079e8bf192bf6fd3d3e6e6c21fe508bcf9e1
David Glasser <glasser@davidglasser.net>
2017-10-02 16:45:58 -0700

    make loop prettier

--- loop.sh
+++ loop.sh
@@ -2,9 +2,10 @@
 
 while true; do
   date
-  echo inserting into 4
-  mongo lighthouse.4.mongolayer.com:10615/testdb -u testuser -p testpass --eval 'db.c.insert({x: "hi"})'
-  echo inserting into 5
-  mongo lighthouse.5.mongolayer.com:10615/testdb -u testuser -p testpass --eval 'db.c.insert({x: "hi"})'
+  echo -n '4: '
+  mongo --quiet lighthouse.4.mongolayer.com:10615/testdb -u testuser -p testpass --eval 'db.c.insert({x: "hi"})'
+  echo -n '5: '
+  mongo --quiet lighthouse.5.mongolayer.com:10615/testdb -u testuser -p testpass --eval 'db.c.insert({x: "hi"})'
+  echo
   sleep 1
 done
