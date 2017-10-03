c3301d3daa83ad6c9ad7530c42a7f2f8d7646d1f
David Glasser <glasser@davidglasser.net>
2017-10-02 18:05:24 -0700

    make it easier to see what is happening
    So it's clear based on changes to packages/mongo that the oplog tail just stops
    getting data.

--- loop.sh
+++ loop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 while true; do
-  date
+  d="$(date)"
+  echo "$d"
   echo -n '4: '
-  mongo --quiet lighthouse.4.mongolayer.com:10615/testdb -u testuser -p testpass --eval 'db.c.insert({x: "hi"})'
+  mongo --quiet lighthouse.4.mongolayer.com:10615/testdb -u testuser -p testpass --eval "db.c.insert({x: '$d'})"
   echo -n '5: '
-  mongo --quiet lighthouse.5.mongolayer.com:10615/testdb -u testuser -p testpass --eval 'db.c.insert({x: "hi"})'
+  mongo --quiet lighthouse.5.mongolayer.com:10615/testdb -u testuser -p testpass --eval "db.c.insert({x: '$d'})"
   echo
   sleep 1
 done
