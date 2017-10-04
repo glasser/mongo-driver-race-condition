561c801a2fc8c7c37e2e852e8d4a362035f8bed4
David Glasser <glasser@davidglasser.net>
2017-10-04 11:33:05 -0700

    install.sh: install mongod and mongo locally

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$(dirname "$0")"
+
+MONGO_VERSION="3.0.11"
+
+# ARCH/OS calculation borrowed from Meteor dev bundle.
+UNAME="$(uname)"
+ARCH="$(uname -m)"
+if [ "$UNAME" == "Linux" ] ; then
+    if [ "$ARCH" != "x86_64" ] ; then
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+
+    OS="linux"
+elif [ "$UNAME" == "Darwin" ] ; then
+    SYSCTL_64BIT=$(sysctl -n hw.cpu64bit_capable 2>/dev/null || echo 0)
+    if [ "$ARCH" == "i386" ] && [ "1" != "$SYSCTL_64BIT" ] ; then
+        # some older macos returns i386 but can run 64 bit binaries.
+        # Probably should distribute binaries built on these machines,
+        # but it should be OK for users to run.
+        ARCH="x86_64"
+    fi
+
+    if [ "$ARCH" != "x86_64" ] ; then
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+
+    OS="osx"
+else
+    echo "Unsupported uname: ${UNAME}"
+fi
+
+echo "Installing Mongo ${MONGO_VERSION}"
+MONGO_NAME="mongodb-${OS}-${ARCH}-${MONGO_VERSION}"
+MONGO_TGZ="${MONGO_NAME}.tgz"
+MONGO_URL="http://fastdl.mongodb.org/${OS}/${MONGO_TGZ}"
+
+rm -rf "${MONGO_NAME}"
+curl "${MONGO_URL}" | tar zx
+mv "${MONGO_NAME}"/bin/mongod .
+mv "${MONGO_NAME}"/bin/mongo .
+rm -rf "${MONGO_NAME}"
--- loop.sh
+++ loop.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd "$(dirname "$0")" || exit
+
 while true; do
   d="$(date)"
   echo "$d"
   echo -n '4: '
-  mongo --quiet lighthouse.4.mongolayer.com:10615/testdb -u testuser -p testpass --eval "db.c.insert({x: '$d'})"
+  ./mongo --quiet lighthouse.4.mongolayer.com:10615/testdb -u testuser -p testpass --eval "db.c.insert({x: '$d'})"
   echo -n '5: '
-  mongo --quiet lighthouse.5.mongolayer.com:10615/testdb -u testuser -p testpass --eval "db.c.insert({x: '$d'})"
+  ./mongo --quiet lighthouse.5.mongolayer.com:10615/testdb -u testuser -p testpass --eval "db.c.insert({x: '$d'})"
   echo
   sleep 1
 done
